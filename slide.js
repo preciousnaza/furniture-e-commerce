9272af4feb35852e162cc84da0cf6f8e3c7b1080
chinaza <onyejekwenaza@gmail.com>
2025-02-11 23:43:20 -0800

    forced commit for furniture-e-commerce

--- slide.js
+++ slide.js
@@ -1,15 +1,25 @@
 
 let next = document.querySelector(".bx-chevron-right");
 let prev = document.querySelector(".bx-chevron-left");
+let items = document.querySelectorAll(".swiper-slide");
 
-next.addEventListener("click", function(){
-    let items = document.querySelectorAll(".swiper-slide");
-    let slide = document.querySelector(".heroslider");
-    slide.appendChild(items[0]);
-})
-prev.addEventListener("click", function(){
-    let items = document.querySelectorAll(".swiper-slide");
-    let slide = document.querySelector(".heroslider");
-    slide.prepend(items[items.length -1]);
+let currentindex = 0;
+
+function showimage(index){
+    items.forEach( (img,i) => {
+        img.classList.toggle("active" , i === index);
+    });
+}
+
+next.addEventListener("click", () =>{
+    currentindex  = (currentindex + 1) % 
+    items.length;
+    showimage(currentindex);
+});
+
+prev.addEventListener("click" , () =>{
+    currentindex = (currentindex - 1 + items.length) %
+    items.length
+    showimage(currentindex);
 })
 
